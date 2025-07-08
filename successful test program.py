bf42c146c4ad4fbfd1efc5feda12e78ea040a850
BlueWorld/Gruxpin <152799656+Abigblueworld@users.noreply.github.com>
2025-07-08 06:19:38 -0400

    The code that started it all. the sentence maker
    I have become the big

---
+++ successful test program.py
@@ -0,0 +1,11 @@
+Woo = ["nully1", "nully2", "nully3", "nully 4"]
+Woo2 = ["nulle1", "nulle2", "nulle3", "nulle4"]
+Finall = ["nulli1", "nulli2", "nulli3", "nulli4"]
+import random
+while (0 == 0):
+  Scoots = random.randrange(0, 3)
+  Scoots2 = random.randrange(0, 3)
+  Scooty = random.randrange(0, 3)
+  Codeliner = Woo[Scoots], Woo2[Scoots2], Finall[Scooty]
+  Jump = input(Codeliner)
+  print("Response was: ''", Jump, "''")
