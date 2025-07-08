ef0558db2794d25722e8bc6fd0fafb0f3807d8e3
BlueWorld/Gruxpin <152799656+Abigblueworld@users.noreply.github.com>
2025-07-08 06:51:34 -0400

    There, that fixed ploblem

--- Procarielbot.py
+++ Procarielbot.py
@@ -1,19 +1,20 @@
-Woo = ["Hi ", "Hey ", "Hello ", "Heya "]
-Woobutswagger = ["Ayo ", "Yo ", "Heyo "]
+Woo = ["Hi, ", "Hey, ", "Hello, ", "Heya, "]
+Woobutswagger = ["Ayo, ", "Yo, ", "Heyo, "]
 Woo2 = ["how are ", "how is ", "", "how's ", "how're "]
 Woo2swagger = ["wazzup?", "what's up?", "what's good?", "how you doing?", "what's going on?"]
 Finall = ["everything?", "your life?", "your day?", "you?", "you doing?"]
 import random
 Jump = input("Input a response to start the bot: ")
+print("You: " + Jump)
 while (0 == 0):
-  if (Woo in Jump):
+  if ("Hi" in Jump or "hi" in Jump or "hello" in Jump or "Hello" in Jump or "Hey" in Jump or "hey" in Jump):
     Scoots = random.randrange(0, 3)
     Scoots2 = random.randrange(0, 3)
     Scooty = random.randrange(0, 4)
     Codeliner = Woo[Scoots] + Woo2[Scoots2] + Finall[Scooty]
-  elif (Woobutswagger in Jump):
+  elif ("ayo" in Jump or "Ayo" in Jump or "Yo" in Jump or "yo" in Jump):
     Scoots = random.randrange(0, 3)
     Scoots2 = random.randrange(0, 3)
     Codeliner = Woobutswagger[Scoots] + Woo2swagger[Scoots2]
-  Jump = str(input(Codeliner))
-  print("You: ''", Jump, "''")
+  print(Codeliner + ":")
+  Jump = input("You: ")
